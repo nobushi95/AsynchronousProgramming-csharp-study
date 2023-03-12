b4e4fd7c6116e806b51130f3b057692d194a6a49
nobushi95 <nobushi1121@gmail.com>
2023-03-12 23:55:05 +0900

    デッドロックする/しないコードの追加

--- DeadLock/MainWindow.xaml.cs
+++ DeadLock/MainWindow.xaml.cs
@@ -2,19 +2,8 @@
 // Licensed under the MIT License.
 
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -31,9 +20,56 @@ namespace DeadLock
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void ThreadCheck_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            var s1 = $"Before Task: {Environment.CurrentManagedThreadId}"; // ID: UIスレッド
+            Task.Run(() =>
+            {
+                var s2 = $"On Task: {Environment.CurrentManagedThreadId}"; // ID: ワーカースレッド
+            }).Wait(); // UIスレッドをロックしない？
+            var s3 = $"After Task: {Environment.CurrentManagedThreadId}"; // ID: UIスレッド
+        }
+
+        private void DeadLockAsyncMethodButton_click(object sender, RoutedEventArgs e)
+        {
+            AsyncMethod().Wait();
+        }
+
+        private void NotDeadLockAsyncMethodButton_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(async () => await AsyncMethod()).Wait();
+        }
+
+        private void DeadLockMethodButton_click(object sender, RoutedEventArgs e)
+        {
+            DeadLockMethod();
+        }
+
+        private void NotDeadLockMethodButton_Click(object sender, RoutedEventArgs e)
+        {
+            NotDeadLockMethod();
+        }
+
+        private static async Task AsyncMethod()
+        {
+            var s1 = $"Before Task: {Environment.CurrentManagedThreadId}";
+            await Task.Run(() =>
+            {
+                var s2 = $"On Task: {Environment.CurrentManagedThreadId}";
+            });
+            var s3 = $"After Task: {Environment.CurrentManagedThreadId}";
+        }
+
+        // Waitを行うためスレッドセーフでないメソッド
+        private static void DeadLockMethod()
+        {
+            AsyncMethod().Wait();
+        }
+
+        // UIスレッドで呼び出されてもデッドロックしないスレッドセーフなメソッド
+        private static void NotDeadLockMethod()
+        {
+            Task.Run(async () => await AsyncMethod()).Wait();
         }
     }
 }
