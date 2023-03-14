f9b1458aa3302d2bfab3a9826e1bb88a41b33fba
nobushi95 <nobushi1121@gmail.com>
2023-03-15 00:20:15 +0900

    デッドロックについてコメントを追加

--- DeadLock/MainWindow.xaml.cs
+++ DeadLock/MainWindow.xaml.cs
@@ -22,6 +22,13 @@ namespace DeadLock
 
         private void ThreadCheck_Click(object sender, RoutedEventArgs e)
         {
+            // 1. s1への代入はUIスレッドで実行される
+            // 2. s2への代入はワーカスレッドで実行される
+            //    UIスレッドはTask(ワーカスレッドで実行されるs2の代入)の終了をWaitする
+            //    ワーカースレッドでの処理が終了すると、UIスレッドのロックを開放（Taskが終了するのでWaitを解除）
+            //    * UIスレッドからワーカスレッドへ遷移した場合は、戻り先のスレッドはワーカースレッド
+            //      ワーカースレッドからワーカースレッドへ遷移した場合の戻り先は呼び出し元のワーカースレッドとは限らない
+            // 3. s3への代入はUIスレッドで実行される
             var s1 = $"Before Task: {Environment.CurrentManagedThreadId}"; // ID: UIスレッド
             Task.Run(() =>
             {
@@ -32,11 +39,28 @@ namespace DeadLock
 
         private void DeadLockAsyncMethodButton_click(object sender, RoutedEventArgs e)
         {
+            // 1. AsyncMethodを呼び出し
+            //    Wait関数を使用しているので、呼び出し元のスレッド(今回はUIスレッド)から他のスレッドに遷移した際に、
+            //    呼び出し元のスレッドをロックする(Task終了をWaitする)
+            // 2. s1への代入はUIスレッドで実行される
+            // 3. s2への代入はワーカースレッドで実行される
+            //    呼び出し元のスレッド(UIスレッド)から他のスレッドに遷移するので、呼び出し元のスレッド(UIスレッド)は
+            //    ロックされる(待機状態になる)
+            //    * AsyncMethod関数が返すTaskの終了を待機するイメージ
+            // 4. s3への代入はワーカースレッドからUIスレッドに戻って実行しようとするが、
+            //    3.でUIスレッドは待機状態に移行している(Taskの終了を待っている)ので、
+            //    処理を実行することができない(デッドロック)
+            //    => UIスレッドでWaitしたTask内の処理でワーカースレッドで処理をしたのちに、
+            //       UIスレッドで処理を行おうとするとデッドロックする
+            //       * Wait()ではなくConfigureAwait(false)とするとUIスレッドに戻らず、
+            //         そのままワーカースレッドで処理が実行されるのでデッドロックしない？？
             AsyncMethod().Wait();
         }
 
         private void NotDeadLockAsyncMethodButton_Click(object sender, RoutedEventArgs e)
         {
+            // ThreadCheck_Click関数と同様に、UIスレッドはTask.Run()の処理がワーカースレッドで実行されるのを待ち、
+            // ワーカースレッドで処理が完了し、待機していたUIスレッドを開放するのでデッドロックしない
             Task.Run(async () => await AsyncMethod()).Wait();
         }
 
@@ -63,12 +87,14 @@ namespace DeadLock
         // Waitを行うためスレッドセーフでないメソッド
         private static void DeadLockMethod()
         {
+            // DeadLockAsyncMethodButton_click関数と同様の動作となる
             AsyncMethod().Wait();
         }
 
         // UIスレッドで呼び出されてもデッドロックしないスレッドセーフなメソッド
         private static void NotDeadLockMethod()
         {
+            // NotDeadLockAsyncMethodButton_Click関数と同様の動作となる
             Task.Run(async () => await AsyncMethod()).Wait();
         }
     }
