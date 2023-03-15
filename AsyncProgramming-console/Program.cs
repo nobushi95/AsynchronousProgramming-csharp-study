29c2469cabb1e58663205f9f6a72a759098a7775
nobushi95 <nobushi1121@gmail.com>
2023-03-15 23:25:22 +0900

    AsyncProgramming_consoleで非同期にできていない非同期関数を実装

--- AsyncProgramming-console/Program.cs
+++ AsyncProgramming-console/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace AsyncProgramming_console
+﻿using System.Diagnostics;
+
+namespace AsyncProgramming_console
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var sw = new Stopwatch();
+            sw.Start();
+            await AsyncMethod();
+            sw.Stop();
+            Console.WriteLine($"AsyncMethod Elapsed:\t{sw.Elapsed}");
+
+            sw.Restart();
+            await NotAsyncMethod();
+            sw.Stop();
+            Console.WriteLine($"NotAsyncMethod Elapsed:\t{sw.Elapsed}");
         }
+
+        private static async Task AsyncMethod()
+        {
+            var list = new List<Task>
+            {
+                Task.Delay(1000),
+                Task.Delay(1000),
+                Task.Run(() => Thread.Sleep(1000)),
+            };
+            await Task.WhenAll(list);
+        }
+
+        // async Taskになっているが非同期に動作していない関数
+        private static async Task NotAsyncMethod()
+        {
+            await Task.Delay(1000);
+            await Task.Delay(1000);
+            await Task.Run(() => Thread.Sleep(1000));
+        }
+
     }
 }
